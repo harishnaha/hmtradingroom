55e139fc1c92de1d6ce54af84f83e64ac3984597
harishnaha <37762231+harishnaha@users.noreply.github.com>
2026-09-08 17:23:27 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'hm-trading-journal-v21';
+const APP_SHELL = './index.html';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(APP_SHELL, copy));
+      return response;
+    }).catch(() => caches.match(APP_SHELL)));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+    return response;
+  })));
+});
